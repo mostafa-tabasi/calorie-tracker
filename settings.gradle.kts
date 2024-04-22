90ac12e60831a2a40e9db5beecf98e323b8361a7
Mostafa Tabasi <dev.mostafatabasi@gmail.com>
2024-04-23 00:27:43 +0800

    chore: Added modules

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -21,4 +21,9 @@ dependencyResolutionManagement {
 
 rootProject.name = "Calorie Tracker"
 include(":app")
- 
+include(":core")
+include(":onboarding:onboarding_presentation")
+include(":onboarding:onboarding_domain")
+include(":tracker:tracker_data")
+include(":tracker:tracker_domain")
+include(":tracker:tracker_presentation")
