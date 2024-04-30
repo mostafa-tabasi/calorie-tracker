abdcb80b44879f3abd6e3572e23cf3ec7435f639
Mostafa Tabasi <dev.mostafatabasi@gmail.com>
2024-04-30 12:56:54 +0800

    chore: Clean-up redundant imports

--- onboarding/onboarding_presentation/src/main/java/com/calorietracker/onboarding_presentation/gender/GenderScreen.kt
+++ onboarding/onboarding_presentation/src/main/java/com/calorietracker/onboarding_presentation/gender/GenderScreen.kt
@@ -8,13 +8,11 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.text.font.FontWeight
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.constraintlayout.compose.ChainStyle
 import androidx.constraintlayout.compose.ConstraintLayout
--- onboarding/onboarding_presentation/src/main/java/com/calorietracker/onboarding_presentation/weight/WeightScreen.kt
+++ onboarding/onboarding_presentation/src/main/java/com/calorietracker/onboarding_presentation/weight/WeightScreen.kt
@@ -3,15 +3,12 @@ package com.calorietracker.onboarding_presentation.weight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.material.MaterialTheme
 import androidx.compose.material.ScaffoldState
-import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
-import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.constraintlayout.compose.ChainStyle
 import androidx.constraintlayout.compose.ConstraintLayout
